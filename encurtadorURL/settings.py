6561fd00c0810d364404bbfd5eccb03fca2dfcf3
Felipe Soares <felipe.soares013@outlook.com>
2022-07-13 16:13:58 -0300

    adionado bd postgres e instalado dependências

--- encurtadorURL/settings.py
+++ encurtadorURL/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iz1d7=^&$1wbiwjy*1a=_k#%_g!&e!sppz9+d8+=u3=d$^!$3k'
+SECRET_KEY = os.getenv('SECRET_KEY', 'adicione-um-segredo-aqui')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'encurtadorURL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
--- url/views.py
+++ url/views.py
@@ -8,7 +8,6 @@ from .models import Links
 # pega a url_curta no banco, para quando for inserida na barra de pesquisa 
 # fazer o redirect para a url original 
 def acha_minha_url(request, url_curta):
-    print(url_curta)
     site = Links.objects.get(url_curta=url_curta)
 
     return redirect(site.url_original)
