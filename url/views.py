7b92324fd54b769a1cf191c692d4a8509b1f6521
tabata Vasques <tabata07defaria@gmail.com>
2022-07-26 16:14:34 -0300

    pagina de erro - restante

--- url/views.py
+++ url/views.py
@@ -6,6 +6,9 @@ from .models import Links
 # pega a url_curta no banco, para quando for inserida na barra de pesquisa
 # fazer o redirect para a url original
 
+def erro(request):
+    return render(request, "url/error.html")
+
 
 def acha_minha_url(request, url_curta):
     site = Links.objects.get(url_curta=url_curta)
